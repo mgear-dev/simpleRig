9086a646c4172fea66cf89ba1ef72eb6a7480434
miquelcampos <miquel.campos@gmail.com>
2021-02-24 11:27:06 +0900

    add menu divider

--- scripts/mgear/simpleRig/menu.py
+++ scripts/mgear/simpleRig/menu.py
@@ -8,6 +8,7 @@ def install():
     pm.setParent(mgear.menu_id, menu=True)
     pm.menuItem(divider=True)
     pm.menuItem(label="Simple Rig Tool", command=str_open_simple_rig)
+    pm.menuItem(divider=True)
 
 
 str_open_simple_rig = """
